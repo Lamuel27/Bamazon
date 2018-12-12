275326847c90b4cceb77bd6b9ab7fa069ed0de6b
Lamuel27 <samrelliner@gmail.com>
2018-12-12 17:00:30 -0600

    restart function needs work

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,6 +11,7 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err
+    console.log("        ______                                             \n        | ___ \\                                            \n        | |_/ /  __ _  _ __ ___    __ _  ____  ___   _ __  \n        | ___ \\ / _` || '_ ` _ \\  / _` ||_  / / _ \\ | '_ \\ \n        | |_/ /| (_| || | | | | || (_| | / / | (_) || | | |\n        \\____/  \\__,_||_| |_| |_| \\__,_|/___| \\___/ |_| |_|\n        ");
     start()
 })
 
@@ -43,7 +44,57 @@ function start() {
             }
         }
     ]).then(function (input) {
+        var item = input.item_id;
+        var count = input.quantity;
 
+        connection.query('SELECT * FROM products WHERE ?', { item_id: item }, function (err, data) {
+            if (err) throw err;
+            else {
+                var itemData = data[0];
 
+                if (count <= itemData.stock_quantity) {
+                    console.log('Your order has been placed! Estimated delivery time is 5 months.\n');
+                    inventory();
+                }
+            }
+
+            function inventory() {
+
+                connection.query('SELECT * FROM products', function (err, data) {
+                    if (err) throw err;
+
+                    console.log('Inventory: ');
+                    console.log('-----------------\n');
+                    var invent = '';
+                    for (var i = 0; i < data.length; i++) {
+                        invent = '';
+                        invent += 'Item ID: ' + data[i].item_id + ' | ';
+                        invent += 'Product Name: ' + data[i].product_name + ' | ';
+                        invent += 'Department: ' + data[i].department_name + ' | ';
+                        invent += 'Price: $' + data[i].price + ' | ';
+                        invent += 'Quantity: ' + data[i].stock_quantity + ' units\n';
+
+                        console.log(invent);
+                    }
+                    restart();
+                })
+            }
+            
+        })   
+    })
+}
+function restart() {
+    inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'item_id',
+            message: 'Would you like to order another item?',
+            }
+    ]).then(function (answers) {
+        if (answers === 'Yes') {
+            start();
+        } else if (answers === 'No') {
+            connection.end();
+        }
     })
 }
